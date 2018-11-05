d7bf7d457ab0a31ce917b488628564f30f9bfd64
MelonieNimasha <36945783+MelonieNimasha@users.noreply.github.com>
2018-11-05 16:59:30 +0530

    Update insert.sql

--- Database/insert.sql
+++ Database/insert.sql
@@ -12,3 +12,9 @@ Insert into bankingAgents values('00007','121212124H','0774212121','Robert Nox',
 Insert into bankingAgents values('00008','121212125H','0775212121','Barak Trump','7,house st,Kandy','details','0008');
 Insert into bankingAgents values('00009','121212126H','0776212121','Jeorge Clinton','6,school st,Galle','details','0009');
 
+insert into customers values('111112222H','Mark Anthony','0711212121','1,forest road,Kohuwala','00004');
+insert into customers values('111113333H','Alan Walker','0712212121','3,jungle road,Horana','00005');
+insert into customers values('111114444H','Justin Beiber','0713212121','5,wood road,Matara','00006');
+insert into customers values('111115555H','Ed sheeran','0714212121','7,cemetry road,Jaffna','00007');
+insert into customers values('111116666H','Shayne Ward','0715212121','9,sea road,Badulla','00008');
+insert into customers values('111117777H','Jennifer Lopez','0716212121','11,ocean road,Galle','00009');
