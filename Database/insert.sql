9890776c997b117d053ca6f798dc25bfbd0bfe24
MelonieNimasha <36945783+MelonieNimasha@users.noreply.github.com>
2018-11-05 16:46:23 +0530

    Update insert.sql

--- Database/insert.sql
+++ Database/insert.sql
@@ -1,2 +1,14 @@
+insert into branches values('0004','Nugegoda');
+insert into branches values('0005','Panadura');
+insert into branches values('0006','Matara');
+insert into branches values('0007','Jaffna');
+insert into branches values('0008','Kandy');
+insert into branches values('0009','Galle');
 
+Insert into bankingAgents values('00004','121212121H','0771212121','Leo Messi','11,flower st,Nugegoda','details','0004');
+Insert into bankingAgents values('00005','121212122H','0772212121','Chris Ronal','10,plant st,Panadura','details','0005');
+Insert into bankingAgents values('00006','121212123H','0773212121','Virat Kohli','9,garden st,Matara','details','0006');
+Insert into bankingAgents values('00007','121212124H','0774212121','Robert Nox','8,tree st,Jaffna','details','0007');
+Insert into bankingAgents values('00008','121212125H','0775212121','Barak Trump','7,house st,Kandy','details','0008');
+Insert into bankingAgents values('00009','121212126H','0776212121','Jeorge Clinton','6,school st,Galle','details','0009');
 
